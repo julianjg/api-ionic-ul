8f3537f16a0c5d8f9c67137c709e53dd9ad2bf3e
julianjg <julian-guzman@upc.edu.co>
2020-05-30 00:02:42 -0500

    quito include de url book

--- apiionic/urls.py
+++ apiionic/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1.0/', include('book.urls')),
+    path('api/v1.0/', ('book.urls')),
 ]
