e2c42dd83abb716cc6fa868ecdef3d56632b14e4
julianjg <julian-guzman@upc.edu.co>
2020-05-30 21:48:11 -0500

    allowed *

--- book/models.py
+++ book/models.py
@@ -9,18 +9,24 @@ class Book(models.Model):
 class CategoriaNegocio(models.Model):
     icon = models.CharField(max_length=50)
     name = models.CharField(max_length=50)
-    redirectTo = models.CharField(max_length=50)
+    redirectTo = models.CharField(max_length=200)
 
 class Panaderia(models.Model):
     icon = models.CharField(max_length=50)
     name = models.CharField(max_length=50) 
-    redirectTo = models.CharField(max_length=50)   
+    redirectTo = models.CharField(max_length=200)   
     image = models.CharField(max_length=70)
 
 class Supermercado(models.Model):
     icon = models.CharField(max_length=50)
     name = models.CharField(max_length=50) 
-    redirectTo = models.CharField(max_length=50)   
+    redirectTo = models.CharField(max_length=200)   
+    image = models.CharField(max_length=70)
+
+class Restaurante(models.Model):
+    icon = models.CharField(max_length=50)
+    name = models.CharField(max_length=50) 
+    redirectTo = models.CharField(max_length=200)   
     image = models.CharField(max_length=70)
 
    
