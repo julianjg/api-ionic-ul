21f0d8f0261f9d0d10c43d44a9346096dc22e0ea
julianjg <julian-guzman@upc.edu.co>
2020-05-30 21:52:59 -0500

    allowed *

--- apiionic/settings.py
+++ apiionic/settings.py
@@ -58,10 +58,10 @@ MIDDLEWARE = [
 CORS_ORIGIN_ALLOW_ALL = True # If this is used then `CORS_ORIGIN_WHITELIST` will not have any effect
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = [
-    'http://localhost:8100',
+    '*',
 ] # If this is used, then not need to use `CORS_ORIGIN_ALLOW_ALL = True`
 CORS_ORIGIN_REGEX_WHITELIST = [
-    'http://nuevaionic.herokuapp.com',
+    '*',
 ]
 
 ROOT_URLCONF = 'apiionic.urls'
