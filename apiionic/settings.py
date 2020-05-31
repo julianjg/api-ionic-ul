df06ebeaf60c15e65ed3b701688fe858c156bfe6
julianjg <julian-guzman@upc.edu.co>
2020-05-30 19:25:52 -0500

    modificar para CORS autorizacion

--- apiionic/settings.py
+++ apiionic/settings.py
@@ -55,14 +55,14 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-#CORS_ORIGIN_ALLOW_ALL = True # If this is used then `CORS_ORIGIN_WHITELIST` will not have any effect
-#CORS_ALLOW_CREDENTIALS = True
-#CORS_ORIGIN_WHITELIST = [
- #   'http://localhost:8100',
-#] # If this is used, then not need to use `CORS_ORIGIN_ALLOW_ALL = True`
-#CORS_ORIGIN_REGEX_WHITELIST = [
-#    'http://localhost:3030',
-#]
+CORS_ORIGIN_ALLOW_ALL = True # If this is used then `CORS_ORIGIN_WHITELIST` will not have any effect
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:8100',
+]  If this is used, then not need to use `CORS_ORIGIN_ALLOW_ALL = True`
+CORS_ORIGIN_REGEX_WHITELIST = [
+    'http://localhost:3030',
+]
 
 ROOT_URLCONF = 'apiionic.urls'
 
