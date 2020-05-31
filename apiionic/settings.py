9f02b5488dad2850fdafea0a5e40e5869e1d6cb1
julianjg <julian-guzman@upc.edu.co>
2020-05-30 19:55:24 -0500

    direccion api en cros

--- apiionic/settings.py
+++ apiionic/settings.py
@@ -58,7 +58,7 @@ MIDDLEWARE = [
 CORS_ORIGIN_ALLOW_ALL = True # If this is used then `CORS_ORIGIN_WHITELIST` will not have any effect
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = [
-    'http://localhost:8100',
+    'http://nuevaionic.herokuapp.com',
 ] # If this is used, then not need to use `CORS_ORIGIN_ALLOW_ALL = True`
 CORS_ORIGIN_REGEX_WHITELIST = [
     'http://localhost:3030',
