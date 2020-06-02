f2b73ed2b858f518f2df040698381cb9d7b6a750
julianjg <julian-guzman@upc.edu.co>
2020-06-01 20:23:14 -0500

    modificando los cors en seting

--- apiionic/settings.py
+++ apiionic/settings.py
@@ -58,7 +58,7 @@ MIDDLEWARE = [
 CORS_ORIGIN_ALLOW_ALL = True # If this is used then `CORS_ORIGIN_WHITELIST` will not have any effect
 #CORS_ALLOW_CREDENTIALS = True
 #CORS_ORIGIN_WHITELIST = [
-    '*',
+  #  '*',
 #] # If this is used, then not need to use `CORS_ORIGIN_ALLOW_ALL = True`
 #CORS_ORIGIN_REGEX_WHITELIST = [
     '*',
