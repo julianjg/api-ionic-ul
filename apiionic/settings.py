3c1748d2b744015a9dab6893c2b23e4b6a7374d9
julianjg <julian-guzman@upc.edu.co>
2020-06-01 20:26:26 -0500

    modificando los cors en seting

--- apiionic/settings.py
+++ apiionic/settings.py
@@ -61,7 +61,7 @@ CORS_ORIGIN_ALLOW_ALL = True # If this is used then `CORS_ORIGIN_WHITELIST` will
   #  '*',
 #] # If this is used, then not need to use `CORS_ORIGIN_ALLOW_ALL = True`
 #CORS_ORIGIN_REGEX_WHITELIST = [
-    '*',
+  # '*',
 #]
 
 ROOT_URLCONF = 'apiionic.urls'
